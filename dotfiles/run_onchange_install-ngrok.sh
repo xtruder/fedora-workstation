8bcbae064aa2fa1a3e10da8783c8d2094064d522
Jaka Hudoklin <jaka@x-truder.net>
2024-12-27 19:19:24 +0100

    dotfiles: ngrok install add check

--- dotfiles/run_onchange_install-ngrok.sh
+++ dotfiles/run_onchange_install-ngrok.sh
@@ -7,3 +7,4 @@ URL="https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz"
 echo "Downloading ngrok from $URL, installing to ~/.local/bin/ngrok"
 curl --progress-bar "$URL" | tar -xz -C ~/.local/bin
 chmod +x ~/.local/bin/ngrok
+ngrok --version
