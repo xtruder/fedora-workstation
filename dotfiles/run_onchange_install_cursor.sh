1810c725fd92cb96ddc63f574b0ae16739b9b7f4
Jaka Hudoklin <jaka@x-truder.net>
2024-12-27 19:54:03 +0700

    fix indent

--- dotfiles/run_onchange_install_cursor.sh
+++ dotfiles/run_onchange_install_cursor.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 DOWNLOAD_URL="https://downloader.cursor.sh/linux/appImage/x64"
 
 # downloads cursor appimage if it doesn't exist yet
-  mkdir -p ~/.local/share/libexec
-  curl -Lo ~/.local/share/libexec/cursor.AppImage "$DOWNLOAD_URL"
-  chmod +x ~/.local/share/libexec/cursor.AppImage
+mkdir -p ~/.local/share/libexec
+curl -Lo ~/.local/share/libexec/cursor.AppImage "$DOWNLOAD_URL"
+chmod +x ~/.local/share/libexec/cursor.AppImage
