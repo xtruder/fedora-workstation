1484f693cdf25838ae3c08949194c47dbeffb5b7
Jaka Hudoklin <jaka@x-truder.net>
2025-04-18 13:23:22 +0700

    Add butane alias

--- dotfiles/dot_bashrc.d/aliases.sh
+++ dotfiles/dot_bashrc.d/aliases.sh
@@ -1,1 +1,6 @@
 alias mpv='flatpak run io.mpv.Mpv'
+
+alias butane='podman run --rm --interactive         \
+              --security-opt label=disable          \
+              --volume "${PWD}:/pwd" --workdir /pwd \
+              quay.io/coreos/butane:release'
